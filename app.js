e81060d37f5ee027b07728a927f2c450a205f0bf
aaayushKr <alwaysayush001@gmail.com>
2023-08-13 17:30:37 +0530

    added login page

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
+
+const csrfProtection = csrf();
 
 const MONGODB_URI =
   "mongodb+srv://Ayush:ayu23@cluster0.wbdk1aa.mongodb.net/shop";
@@ -35,6 +39,8 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -46,6 +52,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use("/admin", adminRoutes);
